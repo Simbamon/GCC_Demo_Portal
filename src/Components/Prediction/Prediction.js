00eee6597590c8f1c7bb482d580fe44d90b293c8
Simbamon <sbhan9511@gmail.com>
2021-12-24 15:43:02 +0900

    prediction added

--- src/Components/Prediction/Prediction.js
+++ src/Components/Prediction/Prediction.js
@@ -3,11 +3,12 @@ import React, { Component } from 'react'
 export class Prediction extends Component {
     
     async componentDidMount() {
-        const test = await fetch('/token')
+        const test = await fetch('/watsonml/token')
         const resp = await test.json()
         const cp4d_token = resp.access_token
-        const data = { token: cp4d_token, url: "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/a7d2373c-7141-42c0-93e1-07b11cc1bec3/predictions?version=2021-10-11" }
-
+        
+        const data = { token: cp4d_token, url: "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/fe6b47dd-4885-48e3-be56-2785fc819317/predictions?version=2021-12-24" }
+        console.log(data)
         const options = {
             method: 'POST',
             headers: {
@@ -16,7 +17,7 @@ export class Prediction extends Component {
             body: JSON.stringify(data)
         }
 
-        const response = await fetch('/test', options)
+        const response = await fetch('/watsonml/test', options)
         const qwer = await response.json()
         console.log(qwer)
     }
