ff78b03b83ba60616c86a7903885e5097f27f33b
Simbamon <sbhan9511@gmail.com>
2022-01-01 12:12:44 +0900

    Disabled wml

--- src/Components/Prediction/Prediction.js
+++ src/Components/Prediction/Prediction.js
@@ -3,30 +3,30 @@ import React, { Component } from 'react'
 export class Prediction extends Component {
     
     async componentDidMount() {
-        const test = await fetch('/watsonml/token')
-        const resp = await test.json()
-        const cp4d_token = resp.access_token
-        const mongo_id = "61c99eccaeb631adea83ac77"
-        const payload = await fetch('/watsonml/payloads/' + mongo_id)
-        const payload_response = await payload.json()
+        // const test = await fetch('/watsonml/token')
+        // const resp = await test.json()
+        // const cp4d_token = resp.access_token
+        // const mongo_id = "61c99eccaeb631adea83ac77"
+        // const payload = await fetch('/watsonml/payloads/' + mongo_id)
+        // const payload_response = await payload.json()
         
 
-        const data = { token: cp4d_token, 
-                       url: "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/fe6b47dd-4885-48e3-be56-2785fc819317/predictions?version=2021-12-24",
-                       input_data: [payload_response]
-                     }
-        console.log(data)
-        const options = {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(data)
-        }
+        // const data = { token: cp4d_token, 
+        //                url: "https://us-south.ml.cloud.ibm.com/ml/v4/deployments/fe6b47dd-4885-48e3-be56-2785fc819317/predictions?version=2021-12-24",
+        //                input_data: [payload_response]
+        //              }
+        // console.log(data)
+        // const options = {
+        //     method: 'POST',
+        //     headers: {
+        //         'Content-Type': 'application/json'
+        //     },
+        //     body: JSON.stringify(data)
+        // }
 
-        const response = await fetch('/watsonml/test', options)
-        const qwer = await response.json()
-        console.log(qwer)
+        // const response = await fetch('/watsonml/test', options)
+        // const qwer = await response.json()
+        // console.log(qwer)
     }
 
     render() {
